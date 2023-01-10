161a177fc0dbc4e6b0d539e2ced618c2d2dd9e13
Aristide <AristideBH@users.noreply.github.com>
2023-01-10 16:05:13 +0100

    Added template for error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="container">
+	<hgroup>
+		<h1>{$page.status}</h1>
+		<h2>{$page.error?.message}</h2>
+	</hgroup>
+</div>
+
+<style>
+	.container {
+		padding-top: 2em;
+	}
+</style>
