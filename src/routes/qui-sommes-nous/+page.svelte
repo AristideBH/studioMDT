3143183a91d1ff911352e3100198febc7c4ca641
Aristide <AristideBH@users.noreply.github.com>
2023-01-10 17:34:17 +0100

    Revert "Clean paages"
    This reverts commit 6b3b6f63f621bd2347089359565463a5ad5704f0.

--- src/routes/qui-sommes-nous/+page.svelte
+++ src/routes/qui-sommes-nous/+page.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+	import DataNumber from '$lib/components/items/DataNumber.svelte';
+</script>
+
 <svelte:head>
 	<title>Titre de la page</title>
 </svelte:head>
@@ -5,9 +9,11 @@
 <section class="container ">
 	<h2>About</h2>
 
-	<p>
+	<p style="height: 200vh">
 		Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus iure assumenda saepe
 		perspiciatis, possimus necessitatibus magni suscipit commodi quibusdam vero itaque mollitia
 		culpa voluptatem inventore odio officia sunt nulla ea?
 	</p>
 </section>
+
+<DataNumber options={{ data: 9000, ico: '/picto/1.png', subtitle: 'text 1' }} />
