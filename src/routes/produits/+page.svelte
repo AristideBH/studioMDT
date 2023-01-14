d51c0178724fd97ed1d2f85aafe82d24a6849947
AristideBH <AristideBH@users.noreply.github.com>
2023-01-14 21:38:09 +0100

    exit pocketbase / add directus

--- src/routes/produits/+page.svelte
+++ src/routes/produits/+page.svelte
@@ -1,25 +1,26 @@
 <script lang="ts">
 	import { slugify } from '$lib/js/utils';
-	import PocketBase from 'pocketbase';
-	const pb = new PocketBase('http://127.0.0.1:8090');
-	// list and filter "example" collection records
-
-	let tofecth = 'products';
-	const allProjects = pb.collection(tofecth).getList(1, 20);
-	const allCategories = pb.collection('categories').getList(1, 20);
 
 	import { slide } from 'svelte/transition';
+
+	import { Directus } from '@directus/sdk';
+	const directus = new Directus('https://n7egmyag.directus.app');
+
+	const products = directus.items('products').readByQuery({
+		sort: ['id']
+	});
+	const categories = directus.items('categories').readByQuery({ limit: 10 });
 </script>
 
 <div class="container">
-	{#await allCategories}
+	{#await categories}
 		<p transition:slide>chargement des filtres...</p>
 	{:then value}
 		<label for="categories">
 			Catégories :
 			<select name="categories" id="categories">
 				<option value="">Tous les projets</option>
-				{#each value.items as item}
+				{#each value.data as item}
 					<option value={item.id}>{item.name}</option>
 				{/each}
 			</select>
@@ -28,16 +29,14 @@
 		<p transition:slide>une erreur s'est produite dans le cgargment des filtres</p>
 		<code>{error}</code>
 	{/await}
-	<div />
 
-	{#await allProjects}
+	{#await products}
 		<p transition:slide>chargement...</p>
 	{:then value}
-		{#each value.items as item}
+		{#each value.data as item}
 			<div transition:slide>
 				<a href="/produits/{slugify(item.title)}">
 					<h2>{item.title}</h2>
-					<img src="http://127.0.0.1:8090/api/files/{tofecth}/{item.id}/{item.vignette}" alt="" />
 				</a>
 			</div>
 		{/each}
