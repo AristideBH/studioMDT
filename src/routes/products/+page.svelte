c0e121b86d704b6729781d9664a1686cb5238d5d
AristideBH <AristideBH@users.noreply.github.com>
2023-01-28 00:09:20 +0100

    Commit

--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
-	import type { PageData } from '../products/$types';
+	import type { PageData } from './$houdini';
 
 	export let data: PageData;
+
+	$: ({ GetAllProducts } = data);
 </script>
 
-<pre>{JSON.stringify(data, undefined, 2)}</pre>
+<!-- <pre>
+	{JSON.stringify($GetAllProducts.data?.products?.edges, undefined, 2)}
+</pre> -->
+
+<section class="container">
+	{#each $GetAllProducts.data?.products?.edges as { node }}
+		<a href={node.uri}>
+			<h2>{node.title}</h2>
+		</a>
+	{/each}
+</section>
